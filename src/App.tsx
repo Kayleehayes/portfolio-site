d09c2411596b71cfa39d72c7cd07cbf045c33a76
Kayleee <Kayleehayes310@gmail.com>
2026-04-25 18:35:02 -0500

    color scheme v.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Features from './pages/Features'
-import PRVisualMedia from './pages/PRVisualMedia'
-import Resume from './pages/Resume'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import Home from './pages/home'
+import Projects from './pages/projects'
+import Features from './pages/features'
+import PRVisualMedia from './pages/prvisualmedia'
+import Resume from './pages/resume'
 import './App.css'
 
 function App() {
    return (
       <HashRouter>
-         <Navbar />
+         <Navbar/>
          <main>
             <Routes>
                <Route path="/" element={<Home />} />
