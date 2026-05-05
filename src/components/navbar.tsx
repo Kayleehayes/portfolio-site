a70ef63dd854a8a8f3d2493ee4164d64054f948c
Kayleee <Kayleehayes310@gmail.com>
2026-05-05 17:21:51 -0500

    simplyfing color and font

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,6 @@ function Navbar() {
          <div className="nav-inner">
             <NavLink to="/" className="nav-name">Kaylee Hayes</NavLink>
             <ul className="nav-links">
-               <li><NavLink to="/" end>Home</NavLink></li>
                <li><NavLink to="/projects">Projects</NavLink></li>
                <li><NavLink to="/features">Features</NavLink></li>
                <li><NavLink to="/pr-visual-media">PR + Visual Media</NavLink></li>
